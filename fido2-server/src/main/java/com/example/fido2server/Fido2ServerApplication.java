ce01f820a68b040a3df7479cf9a6f7712ac96de0
fheller1 <heller.frederik@googlemail.com>
2023-12-12 11:36:36 +0100

    add spring server

---
+++ fido2-server/src/main/java/com/example/fido2server/Fido2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fido2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fido2ServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Fido2ServerApplication.class, args);
+  }
+
+}
---
+++ fido2-server/src/test/java/com/example/fido2server/Fido2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fido2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fido2ServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

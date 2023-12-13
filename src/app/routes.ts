58feea480444d2565f36e82f2cc6564af022e2c0
Frederik Heller <frederik.heller@stud.tu-darmstadt.de>
2023-12-13 17:42:10 +0100

    login component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import {LoginComponent} from "./login/login.component";
 
 const routeConfig: Routes = [
   {
@@ -12,6 +13,11 @@ const routeConfig: Routes = [
     path: 'details/:id',
     component: DetailsComponent,
     title: 'Home details'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Log in'
   }
 ];
 

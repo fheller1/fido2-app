a2a76fc602cb379183ba20b618df93068dcff574
fheller1 <heller.frederik@googlemail.com>
2023-12-14 01:31:44 +0100

    add userService

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,18 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { DetailsComponent } from './details/details.component';
-import {LoginComponent} from "./login/login.component";
+import { LoginComponent } from "./login/login.component";
 
 const routeConfig: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page'
-  },
-  {
-    path: 'details/:id',
-    component: DetailsComponent,
-    title: 'Home details'
   },
   {
     path: 'login',
---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  id!: number;
+  username!: string;
+  firstname!: string;
+  lastname!: string;
+
+  constructor() {
+    this.id = 0;
+    this.username = 'ntesla';
+    this.username = 'Nikola';
+    this.lastname = 'Tesla';
+  }
+}

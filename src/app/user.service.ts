249ae835ef8558de6f6f84271cda88da5601bbdb
fheller1 <heller.frederik@googlemail.com>
2023-12-14 01:31:55 +0100

    add userService

---
+++ src/app/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {User} from "./user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private currentUser: User | null = null;
+  private apiUrl: string = 'localhost:8000/';
+
+  constructor(private router: Router, private http: HttpClient) {
+    this.currentUser = JSON.parse(localStorage.getItem('user')!);
+  }
+
+  create(properties: User): User {
+    const user: User = new User();
+    return user;
+  }
+
+  login(username: string, password: string): void {
+    const user: User = new User();
+    this.currentUser = user;
+    localStorage.setItem('user', JSON.stringify(user));
+    this.router.navigate(['']).then();
+  }
+
+  logout(): void {
+    if(!localStorage.getItem('user')) return;
+    localStorage.removeItem('user');
+    this.currentUser = null;
+    this.router.navigate(['/login']).then();
+  }
+}
